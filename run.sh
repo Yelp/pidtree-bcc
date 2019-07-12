dafbf37a41a2606828032a7da3e06db4188d6311
Matthew Carroll <mattc@yelp.com>
2019-07-12 07:16:10 -0700

    unbroke some stuff

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-./setup.sh
-python2 main.py $@
+$DIR/setup.sh
+python2 $DIR/main.py $@
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+apt-get update
 apt-get -y install linux-headers-$(uname -r)
 mount -t debugfs debugfs /sys/kernel/debug
