8fba178fa3ad2b4f74928c944986f4fa9b98e294
Matt Carroll <oholiab@grimmwa.re>
2019-10-22 13:14:03 +0100

    First PoC at using a ring buffer

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import yaml
 
 from bcc import BPF
 from datetime import datetime
+from functools import partial
 from jinja2 import Template
 from pidtree_bcc import utils
 
@@ -26,6 +27,12 @@ bpf_text = """
 {% endfor %}
 
 BPF_HASH(currsock, u32, struct sock *);
+struct connection_t {
+    u32 pid;
+    u32 daddr;
+    u16 dport;
+}
+BPF_PERF_OUTPUT(events);
 
 int kprobe__tcp_v4_connect(struct pt_regs *ctx, struct sock *sk)
 {
@@ -72,9 +79,13 @@ int kretprobe__tcp_v4_connect(struct pt_regs *ctx)
 
     bpf_probe_read(&saddr, sizeof(saddr), &skp->__sk_common.skc_rcv_saddr);
 
-    bpf_trace_printk("{\\"pid\\": %d, \\"daddr\\": \\"%x\\", \\"dport\\": %d}\\n",
-                     pid, daddr, ntohs(dport));
-    
+    struct connection_t connection = {};
+    connection.pid = pid;
+    connection.port = ntohs(dport);
+    connection.daddr = daddr;
+
+    events.perf_submit(ctx, &connection, sizeof(connection))
+
     currsock.delete(&pid);
 
     return 0;
@@ -102,7 +113,30 @@ def parse_config(config_file):
 def ip_to_int(network):
     """ Takes an IP and returns the unsigned integer encoding of the address """
     return struct.unpack('=L', socket.inet_aton(network))[0]
-    
+
+
+def enrich_event(b, event):
+    proctree_enriched = []
+    error = ""
+    try:
+        proc = psutil.Process(event.pid)
+        proctree = utils.crawl_process_tree(proc)
+        proctree_enriched = list({"pid": p.pid, "cmdline": " ".join(p.cmdline()), "username":  p.username()} for p in proctree)
+    except Exception as e:
+        error=str(e)
+    return(
+        {"timestamp": datetime.utcnow().isoformat() + 'Z',
+        "pid": event.pid,
+        "proctree": proctree_enriched,
+        "daddr": socket.inet_ntoa(struct.pack('<L', int(event.daddr, 16))),
+        "port": event.dport,
+        "error": error})
+
+def print_enriched_event(b, cpu, data, size):
+    event = b["events"].event(data)
+    print >> out, json.dumps(enrich_event(b, event))
+    out.flush()
+
 def main(args):
     config = parse_config(args.config)
     global bpf_text
@@ -115,29 +149,10 @@ def main(args):
         print(expanded_bpf_text)
         sys.exit(0)
     b = BPF(text=expanded_bpf_text)
+    b["events"].open_perf_buffer(partial(print_enriched_event, b))
     with utils.smart_open(args.output_file, mode='w') as out:
         while True:
-            trace = b.trace_readline()
-            # print(trace)
-            proctree_enriched = []
-            error = ""
-            try:
-                # FIXME: this next line isn't right - sometimes there are more colons
-                json_event = trace.split(":", 2)[2:][0]
-                event = json.loads(json_event)
-                proc = psutil.Process(event["pid"])
-                proctree = utils.crawl_process_tree(proc)
-                proctree_enriched = list({"pid": p.pid, "cmdline": " ".join(p.cmdline()), "username":  p.username()} for p in proctree)
-            except Exception as e:
-                error=str(e)
-            print >> out, json.dumps(
-                {"timestamp": datetime.utcnow().isoformat() + 'Z',
-                 "pid": event["pid"],
-                 "proctree": proctree_enriched,
-                 "daddr": socket.inet_ntoa(struct.pack('<L', int(event["daddr"], 16))),
-                 "port": event["dport"],
-                 "error": error})
-            out.flush()
+            b.perf_buffer_poll()
     sys.exit(0)
 
 if __name__ == "__main__":
