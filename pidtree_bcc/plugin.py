215c3d0e08db1eb92f5609c8b502818fe77a3cd0
Matthew Carroll <mattc@yelp.com>
2020-01-28 10:42:09 -0800

    Staticconf works
    Still need to test reloading

--- pidtree_bcc/plugin.py
+++ pidtree_bcc/plugin.py
@@ -4,15 +4,22 @@ class BasePlugin:
     def __init__(self, args):
         self.validate_args(args)
 
+    def process(self, event):
+        """ Process the `event` dict, add in additional metadata and
+        return a dict """
+        raise NotImplementedError("Required method `process` has not been implemented by {}" % self.__name__)
+
     def validate_args(self, args):
+        """ Not required, override in inheriting class if you want to
+        use this """
         pass
 
 
 class Plugins:
     def __init__(self, plugin_dict, plugin_dir="pidtree_bcc.plugins"):
         """ `plugin_dict` is a dict where the keys are plugin names and
-        the value for each key is a nother dict of kwargs. Each key must
-        match a `.py` file in the plugin directory and the kwargs will
+        the value for each key is another dict of kwargs. Each key must
+        match a `.py` file in the plugin directory and the kwargs can
         be validated on initialization of the plugin """
         self._plugins = []
         for plugin_name, plugin_args in plugin_dict.items():
@@ -37,4 +44,5 @@ class Plugins:
                 ))
 
     def plugins(self):
+        """ Return the list of initialized plugin objects """
         return self._plugins
--- pidtree_bcc/plugins/sourceipmap.py
+++ pidtree_bcc/plugins/sourceipmap.py
@@ -1,4 +1,6 @@
 import os
+import staticconf
+from functools import partial
 from pidtree_bcc.plugin import BasePlugin
 
 def hosts_loader(filename):
@@ -11,20 +13,37 @@ def hosts_loader(filename):
         return_dict[splitline[0]] = " ".join(splitline[1:])
     return return_dict
 
+def build_configuration(filename, namespace):
+    config_loader = partial(staticconf.loader.build_loader(hosts_loader),
+                            filename,
+                            namespace=namespace,
+                            flatten=True)
+    reloader = staticconf.config.ReloadCallbackChain(namespace)
+    return staticconf.config.ConfigurationWatcher(config_loader,
+                                                  filename,
+                                                  min_interval=2,
+                                                  reloader=reloader)
+
         
 class Sourceipmap(BasePlugin):
     """ Plugin for mapping source ip to a name """
     def __init__(self, args):
         super
         self.hosts_dict = {}
+        self.config_watchers = []
         self.attribute_key = args.get("attribute_key", "source_host")
         for hostfile in args["hostfiles"]:
-            self.hosts_dict.update(hosts_loader(hostfile))
+            self.config_watchers.append(build_configuration(hostfile, __name__))
+        for config_watcher in self.config_watchers:
+            config_watcher.config_loader()
+        self.config = staticconf.NamespaceReaders(__name__)
 
     def process(self, event):
         saddr = event.get("saddr", None)
+        for config_watcher in self.config_watchers:
+            config_watcher.reload_if_changed()
         if saddr is not None:
-            event[self.attribute_key] = self.hosts_dict.get(saddr, "")
+            event[self.attribute_key] = self.config.read_string(saddr, "")
         return event
 
     def validate_args(self, args):
