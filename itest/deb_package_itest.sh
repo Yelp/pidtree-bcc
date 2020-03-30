ce2f1afea8ffc51b9e662eba49b02efc561945cf
Matthew Carroll <mattc@yelp.com>
2020-03-30 11:01:05 -0700

    More whitespace

--- itest/deb_package_itest.sh
+++ itest/deb_package_itest.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 function setup {
-		apt-get update
-		apt-get -y install lsb-release apt-transport-https
-		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
-		echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" > /etc/apt/sources.list.d/iovisor.list
-		apt-get update
-		apt-get -y install linux-headers-$(uname -r)
-		apt-get -y install /work/dist/*.deb
+  apt-get update
+  apt-get -y install lsb-release apt-transport-https
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
+  echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" > /etc/apt/sources.list.d/iovisor.list
+  apt-get update
+  apt-get -y install linux-headers-$(uname -r)
+  apt-get -y install /work/dist/*.deb
 }
 
 function run {
-		mount -t debugfs debugfs /sys/kernel/debug
-		pidtree-bcc $@
+  mount -t debugfs debugfs /sys/kernel/debug
+  pidtree-bcc $@
 }
 
 CMD=$1
 shift
 if [[ "$CMD" = "setup" ]]; then
-		setup
+  setup
 elif [[ "$CMD" = "run" ]]; then
-		run $@
+  run $@
 else
-		setup
-		run
+  setup
+  run
 fi
