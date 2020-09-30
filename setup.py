8919d4badc02c581697c7e28a84623b73ae31ee4
Matteo Piano <mpiano@yelp.com>
2020-09-30 02:08:36 -0700

    Simplify and improve Debian packaging
    * Generalized packaging docker image
    * setup.py should not import any module code
    * include LICENSE in the build
    * dh-virtualenv should not try running unit tests
    * dh-virtualenv packaging does not require BCC toolkit

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
+import re
 import setuptools
-from pidtree_bcc import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("pidtree_bcc/__init__.py") as fh:
+    version = re.search(r"__version__\s*=\s*\"([^\"]+)", fh.read()).group(1)
+
 setuptools.setup(
     name="pidtree-bcc",
-    version=__version__,
+    version=version,
     author="Matt Carroll",
     author_email="mattc@yelp.com",
     description="eBPF-based intrusion detection and audit logging",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Yelp/pidtree-bcc",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests*', 'itest*', 'packaging*')),
     license='BSD 3-clause "New" or "Revised License"',
     scripts=['bin/pidtree-bcc'],
     classifiers=[
