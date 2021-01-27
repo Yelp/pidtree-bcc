b0d6225b2f60801d4b2f1c1cca64bb11f4d7b330
Matteo Piano <mpiano@yelp.com>
2021-01-27 05:20:22 -0800

    migrate CI to github actions

--- setup.sh
+++ setup.sh
@@ -4,7 +4,12 @@ DEBUG_PATH=/sys/kernel/debug
 INSTALL_ONLY=${INSTALL_ONLY:-false}
 
 apt-get update
-apt-get -y install linux-headers-"$(uname -r)"
+if ! apt-get -y install "linux-headers-$(uname -r)"; then
+    # headers for this specific version are no longer available
+    # we can try to approximate using the metapackage and cross fingers
+    kernel_flavour=$(uname -r | rev | cut -d - -f 1 | rev)
+    apt-get -y install "linux-headers-$kernel_flavour"
+fi
 
 if [ "$INSTALL_ONLY" = "true" ]; then exit 0; fi
 
