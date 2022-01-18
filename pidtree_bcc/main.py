40d4a1c4af246d6f55b3131d650369ffd98f3f25
Matteo Piano <mpiano@yelp.com>
2022-01-18 03:28:09 -0800

    make health-check period configurable

--- pidtree_bcc/main.py
+++ pidtree_bcc/main.py
@@ -23,7 +23,7 @@ from pidtree_bcc.utils import smart_open
 
 
 EXIT_CODE = 0
-HEALTH_CHECK_PERIOD = 60  # seconds
+HEALTH_CHECK_PERIOD_DEFAULT = 60  # seconds
 HANDLED_SIGNALS = (signal.SIGINT, signal.SIGTERM)
 
 
@@ -50,6 +50,10 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         '-w', '--watch-config', action='store_true', default=False,
         help='Enable configuration file watch and hot-swapping for probe network filters',
+    )
+    parser.add_argument(
+        '--health-check-period', type=int, default=HEALTH_CHECK_PERIOD_DEFAULT,
+        help='Controls how often the watchdog thread performs health and configuration checks (in seconds)',
     )
     parser.add_argument(
         '--lost-event-telemetry', type=int, default=-1, metavar='NEVENTS',
@@ -106,18 +110,20 @@ def health_and_config_watchdog(
     probe_workers: List[Process],
     output_fh: TextIO,
     config_watcher: ConfigurationWatcher = None,
+    check_period: int = HEALTH_CHECK_PERIOD_DEFAULT,
 ):
     """ Check that probe processes are alive, output file is writable and monitor configuration changes
 
     :param List[Process] probe_workers: list of probe processes
     :param TextIO output_fh: Output file handle
     :param ConfigurationWatcher config_watcher: Watcher for monitoring configuration changes
+    ;param int check_period: how often the checks are run (in seconds)
     """
     global EXIT_CODE
     fs_poller = select.poll()
     fs_poller.register(output_fh, select.POLLERR)
     while True:
-        time.sleep(HEALTH_CHECK_PERIOD)
+        time.sleep(check_period)
         bad_fds = fs_poller.poll(0)
         if not all(worker.is_alive() for worker in probe_workers) or bad_fds:
             EXIT_CODE = 1
@@ -140,7 +146,11 @@ def main(args: argparse.Namespace):
     curried_handler = partial(termination_handler, probe_workers)
     for s in HANDLED_SIGNALS:
         signal.signal(s, curried_handler)
-    config_watcher = setup_config(args.config, watch_config=args.watch_config)
+    config_watcher = setup_config(
+        args.config,
+        watch_config=args.watch_config,
+        min_watch_interval=args.health_check_period,
+    )
     out = smart_open(args.output_file, mode='w')
     output_queue = SimpleQueue()
     probes = load_probes(
@@ -161,7 +171,7 @@ def main(args: argparse.Namespace):
         probe_workers[-1].start()
     watchdog_thread = Thread(
         target=health_and_config_watchdog,
-        args=(probe_workers, out, config_watcher),
+        args=(probe_workers, out, config_watcher, args.health_check_period),
         daemon=True,
     )
     watchdog_thread.start()
